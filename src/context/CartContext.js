cb6c2799078ee05ef1fed14ad9e7e72dbca0ba25
NRNagaphanideep <phanideep14@gmail.com>
2023-02-26 14:25:05 +0000

    miniproject

---
+++ src/context/CartContext.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CartContext = React.createContext({
+  cartList: [],
+  removeAllCartItems: () => {},
+  addCartItems: () => {},
+  removeCartItem: () => {},
+  incrementCartItemQuantity: () => {},
+  decrementCartItemQuantity: () => {},
+})
+
+export default CartContext
